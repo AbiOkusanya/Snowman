40a349cc29b7e74a46425e97551f78e44f442311
AbiOkusanya <73357072+AbiOkusanya@users.noreply.github.com>
2020-12-14 15:48:49 -0500

    my commit

---
+++ js/project1.js
@@ -0,0 +1,11 @@
+
+/*need function to make button work, need timer?*/
+
+function startGame (){
+    
+}
+
+/* need an array of random words
+somehow make underscores to match length of words
+right guessed letters will push to correct section
+wrong guessed letters should push to incorrect section*/
